24ecb0dbbd5adcc11e808902081832f600d9e493
Ariel Altaras <ariel.altaras@gmail.com>
2018-05-16 19:23:32 -0700

    fixed indentation

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,13 +1,13 @@
-const greet = require('../lib/greet.js')
+const greet = require('../lib/greet.js');
 
 describe('greet module', () => {
-    it('should return null when passed something other than a string', () => {
-        expect(greet()).toBeNull();
-    })
+  it('should return null when passed something other than a string', () => {
+    expect(greet()).toBeNull();
+  });
 
-    it('should return "Hello, Brian" when passed Brian', () => {
-        expect(greet('Brian')).toBe('Hello, Brian');
-    }
+  it('should return "Hello, Brian" when passed Brian', () => {
+    expect(greet('Brian')).toBe('Hello, Brian');
+  }
 
-    )
-})
+  );
+});
